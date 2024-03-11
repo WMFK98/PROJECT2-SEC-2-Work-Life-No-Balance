d8e65f123d1e26068d21939a414530197847cac6
realFK98 <wishira2013.coth@gmail.com>
2024-03-12 00:25:42 +0700

    remove

--- src/components/SwitchSideLower.vue
+++ src/components/SwitchSideLower.vue
@@ -21,57 +21,43 @@ const props = defineProps([
     "
   >
     <slot name="currentPoint"></slot>
-    <!-- <div
-      id="cp-p1"
-      class="ml-[47px] scr-l:ml-0 mr-auto w-[108px] h-[69px] rounded-[20px] flex flex-col justify-center items-center scr-m:h-[101.823px] scr-m:w-[162.763px] scr-l:w-[211px] scr-l:h-[132px]"
-      :class="
-        theWinner === player
-          ? ' bg-Yellow text-Black'
-          : 'bg-Main-pink-300 text-White'
-      "
-    >
-      <p class="text-hss scr-m:text-hs-tal scr-l:text-hs-des">CURRENT</p>
-      <p class="text-hs scr-l:text-hm-des scr-m:text-hm-tal">
-        {{ player.curPoint }}
-      </p>
-    </div> -->
-    <div class="flex flex-col gap-1">
-      <slot name="itemsBar"></slot>
-      <!-- <p class="text-Black px-1 scr-m:text-hs-tal scr-l:text-hs-des">ITEMS</p> -->
 
-      <div
-        id="items-p1 p-[2px]"
-        class="scr-l:w-[480px] w-auto h-[45px] rounded-[10px] flex p-1 gap-1 bg-White text-hss scr-m:text-hs-tal scr-l:text-hs-des text-White scr-m:h-[63.49px] scr-l:h-[71px] scr-m:rounded-[20px]"
-      >
-        <label
-          v-for="{
-            id,
-            isUsed,
-            itemInfo: { name, isPerTurn, isAttack, picture },
-          } in player.items.getAllItem()"
-          :key="id"
-          class="swap swap-rotate item btn text-hss scr-l:text-hs-des scr-m:text-hs-tal btn-sm border-0 rounded-[10px] w-[38px] scr-l:w-[64px] scr-m:w-[57.49px] scr-m:rounded-[20px] h-auto items-center p-[1px]"
-          :class="
-            !(currentPlayer[0] === player) || theWinner
-              ? 'bg-btn-hover  text-White'
-              : isUsed
-              ? 'bg-Yellow-light  text-Black'
-              : isPerTurn || name === 'Dice+' || name === 'Dice-'
-              ? 'bg-isTurn text-White'
-              : isAttack
-              ? 'bg-Main-pink-300 text-White'
-              : 'bg-isPerTurn text-White'
-          "
+    <div class="flex flex-col gap-1">
+      <slot name="items-bar">
+        <div
+          id="items-p1 p-[2px]"
+          class="scr-l:w-[480px] w-auto h-[45px] rounded-[10px] flex p-1 gap-1 bg-White text-hss scr-m:text-hs-tal scr-l:text-hs-des text-White scr-m:h-[63.49px] scr-l:h-[71px] scr-m:rounded-[20px]"
         >
-          <input
-            @click="player.items.toggleUsedItem(id, isPlaySoundSF)"
-            type="checkbox"
-            :disabled="!(currentPlayer[0] === player) || theWinner"
-          />
-          <img class="swap-off" :src="picture" />
-          <img class="swap-on" :src="picture" />
-        </label>
-      </div>
+          <label
+            v-for="{
+              id,
+              isUsed,
+              itemInfo: { name, isPerTurn, isAttack, picture },
+            } in player.items.getAllItem()"
+            :key="id"
+            class="swap swap-rotate item btn text-hss scr-l:text-hs-des scr-m:text-hs-tal btn-sm border-0 rounded-[10px] w-[38px] scr-l:w-[64px] scr-m:w-[57.49px] scr-m:rounded-[20px] h-auto items-center p-[1px]"
+            :class="
+              !(currentPlayer[0] === player) || theWinner
+                ? 'bg-btn-hover  text-White'
+                : isUsed
+                ? 'bg-Yellow-light  text-Black'
+                : isPerTurn || name === 'Dice+' || name === 'Dice-'
+                ? 'bg-isTurn text-White'
+                : isAttack
+                ? 'bg-Main-pink-300 text-White'
+                : 'bg-isPerTurn text-White'
+            "
+          >
+            <input
+              @click="player.items.toggleUsedItem(id, isPlaySoundSF)"
+              type="checkbox"
+              :disabled="!(currentPlayer[0] === player) || theWinner"
+            />
+            <img class="swap-off" :src="picture" />
+            <img class="swap-on" :src="picture" />
+          </label>
+        </div>
+      </slot>
     </div>
   </div>
 </template>
