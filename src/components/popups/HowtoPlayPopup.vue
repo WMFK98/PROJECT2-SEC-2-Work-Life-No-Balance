c0561ebec0c3107797648e6cd62d7faad1428d46
realFK98 <wishira2013.coth@gmail.com>
2024-03-31 17:36:34 +0700

    hot fix

--- src/components/popups/HowtoPlayPopup.vue
+++ src/components/popups/HowtoPlayPopup.vue
@@ -51,20 +51,20 @@ defineProps({ id: String });
               v-show="selectTutorialPage === 2 || selectTutorialPage === 3"
               class="flex gap-3 text-hss scr-l:text-hs-des"
             >
-              <div class="flex gap-3 text-hss scr-l:text-hs-des">
-                <div class="flex gap-2 src-l:gap-4">
+              <div class="flex gap-3 text-hss ml-2 scr-l:text-hs-des">
+                <div class="flex gap-2 items-center src-l:gap-4">
                   <div
                     class="bg-item-time w-4 h-4 scr-l:w-8 scr-l:h-8 rounded-[5px]"
                   ></div>
                   <p>Item Time</p>
                 </div>
-                <div class="flex gap-2">
+                <div class="flex gap-2 items-center">
                   <div
                     class="bg-item-turn w-4 h-4 scr-l:w-8 scr-l:h-8 rounded-[5px]"
                   ></div>
                   <p>Item Turn</p>
                 </div>
-                <div class="flex gap-2">
+                <div class="flex gap-2 items-center">
                   <div
                     class="bg-Main-pink-300 w-4 h-4 scr-l:w-8 scr-l:h-8 rounded-[5px]"
                   ></div>
